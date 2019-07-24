e9b7d8515bd3c38c91bca624ccff0d3967fc6c54
mubarak23 <mubarakaminu340@gmail.com>
2019-07-25 00:46:11 +0100

    add the sidebar to app.js file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar'
 
 function App() {
   return (
     <div>
       <Header />
+      <Sidebar />
     </div>
   );
 }
