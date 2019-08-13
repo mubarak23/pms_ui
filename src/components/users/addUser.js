ebe96dc1f335a9d49d346654e21c5785221f8add
mubarak23 <mubarakaminu340@gmail.com>
2019-08-13 12:33:23 +0100

    create the onchage functions and bind this keyword

--- src/components/users/addUser.js
+++ src/components/users/addUser.js
@@ -5,7 +5,66 @@ import Navbar from '../../../src/views/Home/superNav';
 class Adduser extends React.Component{
     constructor(props){
         super(props);
+
+        //binding
+        this.onchangeUsername = this.onchangeUsername.bind(this);
+        this.onchangePassword = this.onchangePassword.bind(this);
+        this.onchangeEmail = this.onchangeEmail.bind(this);
+        this.onchageAssign_to = this.onchageAssign_to.bind(this);
+        this.onchangeReg_no = this.onchangeReg_no.bind(this);
+
+        this.state = {
+            username: '',
+            password: '',
+            email: '',
+            assign_to: '',
+            reg_no: '',
+            status: ''
+        }
     }
+
+    onchangeUsername = (e) =>{
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onchangePassword = (e) =>{
+        this.setState({
+            password: e.target.value
+        });
+    }
+    onchangeEmail = (e) =>{
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    onchageAssign_to = (e) =>{
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    onchangeReg_no = (e) =>{
+        this.setState({
+            reg_no: e.target.value
+        });
+    } 
+
+    onSubmit = (e) =>{
+        e.preventDefault();
+        const addUser = {
+            username: this.state.username,
+            password: this.state.password,
+            email: this.state.email,
+            reg_no: this.state.reg_no,
+            assign_to: this.state.assign_to
+        };
+
+        console.log(addUser);
+    }
+
 
     render(){
         return(
