e808c13695c84a9285426f3868c91f68530b960c
mubarak23 <mubarakaminu340@gmail.com>
2019-08-13 13:20:28 +0100

    create the creating meeting form with time and date picker

--- src/components/meetings/addMeeting.js
+++ src/components/meetings/addMeeting.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-datepicker/dist/react-datepicker.css";
+import TimePicker from 'react-time-picker'
 import Navbar from "../../../src/views/Home/superNav";
 
 class AddMeeting extends React.Component{
@@ -10,9 +12,17 @@ class AddMeeting extends React.Component{
             time: '',
             location: '',
             with_student: '',
-            created_by: ''
+            created_by: '',
+            users: []
         }
     };
+
+    componentDidMount(){
+        this.setState({
+            users : ['Mubarak Aminu'],
+            username : ['Mubarak Aminu'],
+        })
+    }
 
     onChangeDate = (e) =>{
         this.setState({
@@ -60,7 +70,59 @@ class AddMeeting extends React.Component{
         return(
             <div>
                 <Navbar/>
-                Component that allow us to create schedule meeting our supervisor
+                <div className="container">
+                <h3>Create Meeting Schedule</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                       <label>Location</label>
+                       <input type="text" 
+                        required
+                        className="form-control"
+                        value={this.state.location}
+                        onChange = {this.onChangeLocation} />
+                    </div>
+                    <div className="form-group">
+                       <label>Date</label>
+                        <div>
+                        <DatePicker 
+                        selected={this.state.date}
+                        onChange={this.onChangeDate}/>
+                        </div>   
+                    </div>
+                    
+                    <div className="form-group">
+                       <label>Date</label>
+                        <div>
+                        <TimePicker 
+                        selected={this.state.time}
+                        onChange={this.onChangeTime}/>
+                        </div> 
+                    </div>
+                    <div className="form-group">
+                    <label>UserName</label>
+                       <select ref="userInput"
+                       required
+                       className="form-control"
+                       value={this.state.with_student}
+                       onChange ={this.onChangeWithStudent}>
+                           {
+                               this.state.users.map(function(user){
+                                   return <option key={user}
+                                   value={user}>{user}    
+                                   </option>;
+                               })
+                           }
+                       </select> 
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" 
+                        required
+                        className="btn btn-primary"
+                        value="Create Meeting Schedule"
+                         />   
+                    </div>
+                </form>
+                </div>
             </div>
         )
     }
